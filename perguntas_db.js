b6413556cfc0823db9c20ea6f08103021c311fbf
linus20245 <167362775+linus20245@users.noreply.github.com>
2024-04-23 18:20:24 -0300

    Update perguntas_db.js

--- perguntas_db.js
+++ perguntas_db.js
@@ -1,61 +1,133 @@
 // perguntas_db.js
 const perguntasDB = [
-{
-    pergunta: "Qual fruta é conhecida como 'fruta dos anjos'?",
-    respostas: {
-        a: "Manga",
-        b: "Carambola"
+ {
+        "pergunta": "Qual é o maior animal terrestre?",
+        "respostas": {
+            "a": "Elefante africano",
+            "b": "Girafa"
+        },
+        "correta": "a"
     },
-    correta: "a"
-},
-{
-    pergunta: "Qual é o vegetal mais consumido no mundo?",
-    respostas: {
-        a: "Cenoura",
-        b: "Batata"
+    {
+        "pergunta": "Qual planeta é conhecido como o 'Planeta Vermelho'?",
+        "respostas": {
+            "a": "Vênus",
+            "b": "Marte"
+        },
+        "correta": "b"
     },
-    correta: "b"
-},
-{
-    pergunta: "Qual é o prato nacional do Japão?",
-    respostas: {
-        a: "Sushi",
-        b: "Tempurá"
+    {
+        "pergunta": "Qual é o elemento químico mais leve?",
+        "respostas": {
+            "a": "Hidrogênio",
+            "b": "Hélio"
+        },
+        "correta": "a"
     },
-    correta: "a"
-},
-{
-    pergunta: "Qual é o tipo de carne usado tradicionalmente em um hambúrguer?",
-    respostas: {
-        a: "Frango",
-        b: "Carne bovina"
+    {
+        "pergunta": "Qual cidade é conhecida como a 'Cidade dos Ventos'?",
+        "respostas": {
+            "a": "Chicago",
+            "b": "Boston"
+        },
+        "correta": "a"
     },
-    correta: "b"
-},
-{
-    pergunta: "Qual é o ingrediente principal do guacamole?",
-    respostas: {
-        a: "Abacate",
-        b: "Tomate"
+    {
+        "pergunta": "Qual é o livro mais vendido no mundo depois da Bíblia?",
+        "respostas": {
+            "a": "O Senhor dos Anéis",
+            "b": "Dom Quixote"
+        },
+        "correta": "b"
     },
-    correta: "a"
-},
-{
-    pergunta: "Qual é o país de origem da pizza?",
-    respostas: {
-        a: "Itália",
-        b: "Estados Unidos"
+    {
+        "pergunta": "Qual é o oceano mais profundo do mundo?",
+        "respostas": {
+            "a": "Atlântico",
+            "b": "Pacífico"
+        },
+        "correta": "b"
     },
-    correta: "a"
-},
-{
-    pergunta: "Qual é o nome da massa italiana em forma de pequenas bolinhas?",
-    respostas: {
-        a: "Spaghetti",
-        b: "Gnocchi"
+    {
+        "pergunta": "Qual língua é a mais falada no mundo?",
+        "respostas": {
+            "a": "Inglês",
+            "b": "Mandarim"
+        },
+        "correta": "b"
     },
-    correta: "b"
-},
+    {
+        "pergunta": "Qual país tem o maior número de pirâmides?",
+        "respostas": {
+            "a": "Egito",
+            "b": "Sudão"
+        },
+        "correta": "b"
+    },
+    {
+        "pergunta": "Qual é a capital da Austrália?",
+        "respostas": {
+            "a": "Sydney",
+            "b": "Canberra"
+        },
+        "correta": "b"
+    },
+    {
+        "pergunta": "Que animal é conhecido por ter a mordida mais forte do reino animal?",
+        "respostas": {
+            "a": "Crocodilo",
+            "b": "Tubarão branco"
+        },
+        "correta": "a"
+    },
+    {
+        "pergunta": "Qual é a peça de teatro mais longa em performance?",
+        "respostas": {
+            "a": "Os Miseráveis",
+            "b": "A Ratoeira"
+        },
+        "correta": "b"
+    },
+    {
+        "pergunta": "Qual país é conhecido como a terra do sol nascente?",
+        "respostas": {
+            "a": "Japão",
+            "b": "China"
+        },
+        "correta": "a"
+    },
+    {
+        "pergunta": "Qual é a substância mais dura conhecida pelo homem?",
+        "respostas": {
+            "a": "Diamante",
+            "b": "Grafeno"
+        },
+        "correta": "a"
+    },
+    {
+        "pergunta": "Qual destas cidades antigas foi redescoberta em 1748 após estar enterrada sob cinzas vulcânicas?",
+        "respostas": {
+            "a": "Machu Picchu",
+            "b": "Pompeia"
+        },
+        "correta": "b"
+    },
+    {
+        "pergunta": "Qual é a maior floresta tropical do mundo?",
+        "respostas": {
+            "a": "Floresta do Congo",
+            "b": "Amazônia"
+        },
+        "correta": "b"
+    },
+    {
+        "pergunta": "Qual destes elementos é necessário para a fabricação de vidro?",
+        "respostas": {
+            "a": "Silício",
+            "b": "Carbono"
+        },
+        "correta": "a"
+    },
 
 
     // Adicione mais perguntas aqui
